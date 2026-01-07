2e247a8e7e55b0fb7b70e7483963fd4b66d2310f
scrape-squad-test <scraptest2025@outlook.com>
2026-01-06 18:31:32 -0600

    Create print_words.py

---
+++ print_words.py
@@ -0,0 +1,12 @@
+file_path = 'text_file.txt'  # Replace with file name 
+
+try:
+    # read mode ('r')
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+        print("--- File Content ---")
+        print(file_content)
+except FileNotFoundError:
+    print(f"Error: The file '{file_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
