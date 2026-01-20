b2515d008dcf232cc64b66aa0026d6d36c89b568
scrape-squad-test <scraptest2025@outlook.com>
2026-01-20 15:34:16 -0600

    Update print_words.py

--- print_words.py
+++ print_words.py
@@ -10,3 +10,5 @@ except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
+
+# The University of Alabama at Birmingham (University of Alabama at Birmingham) UAB
