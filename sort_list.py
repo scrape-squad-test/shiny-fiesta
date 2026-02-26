e7ef3a39854d6b13d547c42f6d811ed303afe9f3
scrape-squad-test <scraptest2025@outlook.com>
2026-02-26 13:39:08 -0600

    Update sort_list.py added comment

--- sort_list.py
+++ sort_list.py
@@ -1,4 +1,5 @@
 # Sorts the terms uabsports.com , uab.edu , app.uabsports.com , go.uabsports.com , UAB , xn--b1aaa.cis.uab.edu , and img.uabsports.com alphabetically then prints them 
+# m.uabsports.com , mail01.go.uabsports.com , pp.uabsports.com , shop.uabsports.com , t.uabsports.com , www.uabsports.com , a.ebscohost.com.fetch.mhsl.uab.edu
 
 list = ["uabsports.com" , "uab.edu" , "app.uabsports.com" , "go.uabsports.com", "UAB" , "xn--b1aaa.cis.uab.edu" , "img.uabsports.com" ]
 
