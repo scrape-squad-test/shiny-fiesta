bdc90cb63bbf807dae1675d796eb7eebf87a0da4
scrape-squad-test <scraptest2025@outlook.com>
2026-02-26 13:36:00 -0600

    Create sort_list.py to sort a list alphabetically

---
+++ sort_list.py
@@ -0,0 +1,7 @@
+# Sorts the terms uabsports.com , uab.edu , app.uabsports.com , go.uabsports.com , UAB , xn--b1aaa.cis.uab.edu , and img.uabsports.com alphabetically then prints them 
+
+list = ["uabsports.com" , "uab.edu" , "app.uabsports.com" , "go.uabsports.com", "UAB" , "xn--b1aaa.cis.uab.edu" , "img.uabsports.com" ]
+
+
+list.sort()
+print(list) 
